364c18e61570c6dd520d708b33909f14fbbd8d51
SrinivasPolaveni <srinivasyadavpolaveni@gmail.com>
2022-10-13 16:34:18 +0000

    Jobby App

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import {Route, Switch, Redirect} from 'react-router-dom'
+import Login from './components/Login'
+import Home from './components/Home'
+import NotFound from './components/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
+import AboutJobItem from './components/AboutJobItem'
+import Jobs from './components/Jobs'
+import './App.css'
+
+const App = () => (
+  <Switch>
+    <Route exact path="/login" component={Login} />
+    <ProtectedRoute exact path="/" component={Home} />
+    <ProtectedRoute exact path="/jobs" component={Jobs} />
+    <ProtectedRoute exact path="/jobs/:id" component={AboutJobItem} />
+    <Route path="/not-found" component={NotFound} />
+    <Redirect to="not-found" />
+  </Switch>
+)
+
+export default App
---
+++ src/components/Home/index.js
@@ -0,0 +1,37 @@
+import {Link} from 'react-router-dom'
+import Header from '../Header'
+import './index.css'
+
+const Home = props => {
+  const onRedirectToJobs = () => {
+    const {history} = props
+    history.replace('/jobs')
+  }
+
+  return (
+    <>
+      <Header />
+      <div className="home-container">
+        <h1 className="home-heading">
+          Find The Job That <br />
+          Fits Your Life
+        </h1>
+        <p className="home-paragraph">
+          Millions of people are searching for jobs, salary information, company
+          reviews. Find the job that fits your abilities and potential.
+        </p>
+        <Link className="retry-btn-link" to="/jobs">
+          <button
+            className="home-jobs-button"
+            type="button"
+            onClick={onRedirectToJobs}
+          >
+            Find Jobs
+          </button>
+        </Link>
+      </div>
+    </>
+  )
+}
+
+export default Home
